cf633b334f1bd69fe1e87767305d7c93f3f71059
Akhil Goyal <agoyal2_be20@thapar.edu>
2023-07-25 03:46:43 +0530

    add cart page done

--- server.js
+++ server.js
@@ -6,24 +6,22 @@ const expressLayout = require('express-ejs-layouts');
 
 const port = process.env.port || 3000;
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
 
 //set template engine
-
+app.use(express.static('assets'));
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
 
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
-
-
-
-
-
-
---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+let mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'assets/js/app.js').sass('resources/scss/app.scss', 'assets/css/app.css');
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+});
